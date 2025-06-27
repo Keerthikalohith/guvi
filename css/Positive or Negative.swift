1ababbe79f8f3494a7a7e4f8c9dee0c74cca91b4
Keerthikalohith <keerthisakthi222@gmail.com>
2025-06-27 18:37:07 +0530

    First commit

---
+++ css/Positive or Negative.swift
@@ -0,0 +1,50 @@
+Positive or Negative
+Requirement
+1.positive-0,1,2,3.....
+2.Negative- -1,-2,-3......
+Maths Logic
+positive = Number>0 then number is positive number
+Negative= Number<0 then number is Negative number
+input: number--->eg:1,2,3....
+outpuut:number--->eg:-1,-2,-3.....
+flow:
+1.get user input assign variable number
+2.if number>0 print Positive
+3.else if number<0 print Negative
+
+
+current Bill
+-----------
+Requirement
+1unit= 1.5rs
+Maths Logic
+price=unit * 1.5
+input: no of unit
+output:Current bill amount
+flow:
+get user unit input
+and multiply unit with per unit price
+then print current bill
+ Requirement2 flow:
+ get user unit input
+ ---------------------
+ then unit shoud be 100 or equal to hunderad multiply with 1.5 rupees 
+ or unit shoud be more than 100,subtract the actual current bill with 100
+ then mutiply the remaining amount with 2.5 and add both values and produce current bill to end user
+
+ TN electicity bill
+ ------------------
+Requirement: 
+   Unit 1--->100 =0
+   unit 100-->200=2.35
+   unit 200-->400=4.7
+
+   input: user input
+   output:100 unit user bill shoud be"you dont have any bill! u comes under the goverment privellege "
+           more than 100 0r below 200 it shoud be " thank you! your bill amount"
+           more than 200 0r bepow 400 it shoud be "thank you! your bill amount"
+     flow
+     ------> get user input
+     ------>if it comes under or equal to 100 unit print 0 store in a variable
+     .......>it unit<=200--->subtract with 100 unit...>remaining unit we have to multiply with unit*2.35
+     ........>if unit 400---->subtract with  100 unit ....>remaining we haveto multiply with 4.7+add with 100*2.35
